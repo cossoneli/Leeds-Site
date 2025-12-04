7c6c75ab5bcd6c6cf557716b83192505b6f84123
cossoneli <cossoneli@gmail.com>
2025-12-04 09:02:59 -0600

    forgot to fix routes on css and js files

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
     // IF A PANEL IS CLICKED, GO TO THREAD PAGE
     $(".my-panel").click(function() {
         const topic = $(this).data('topic'); // get data-topic
-        window.open('/LeedsSite/views/thread.php?topic=' + topic, '_blank'); 
+        window.open('/index.php?page=thread&topic=' + topic, '_blank'); 
     })
 
     $(document).on('click', '.reply-button', function() {
